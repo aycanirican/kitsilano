4fa6d8437f65d498e8eb4eb80175c2b2c05e7eae
Aycan iRiCAN <iricanaycan@gmail.com>
2019-04-18 01:16:57 +0300

    i18n support

--- docker/default.nix
+++ docker/default.nix
@@ -1,22 +1,23 @@
 { pkgs, emacsEnv }:
+with pkgs;
 
 let
   version = "latest";
-  baseImage = pkgs.dockerTools.buildImage {
+  baseImage = dockerTools.buildImage {
     name = "baseImage";
     tag = version;
-    contents = with pkgs; [ bashInteractive cacert busybox curl git ];
+    contents = [ bashInteractive glibcLocales cacert busybox curl git ];
     # maxLayers = 120;
   };
 
 in
-{ emacsImage = pkgs.dockerTools.buildImage {
+{ emacsImage = dockerTools.buildImage {
     name = "emacs";
     tag  = version;
     fromImage = baseImage;
-    contents = with pkgs; [ emacsEnv mu isync gnupg ];
+    contents = [ emacsEnv mu isync gnupg silver-searcher ];
     runAsRoot = ''
-      ${pkgs.dockerTools.shadowSetup}
+      ${dockerTools.shadowSetup}
       echo "tcp	6	TCP\nudp 17      UDP" >> /etc/protocol
       echo "hosts: files dns myhostname mymachines" > /etc/nsswitch.conf
       mkdir /tmp && chmod 1777 /tmp
@@ -25,7 +26,9 @@ in
       chown -R user:users /home/user
     '';
     config = {
-      Cmd = [ "${pkgs.gosu.bin}/bin/gosu" "user" "${emacsEnv}/bin/emacs" ];
+      Cmd = [ "${gosu.bin}/bin/gosu" "user" "${emacsEnv}/bin/emacs" ];
+      Env = [ "LANG=en_US.UTF-8"
+              "LOCALE_ARCHIVE=${glibcLocales}/lib/locale/locale-archive" ];
       WorkingDir = "/home/user/data";
       Volumes = { };
     };
