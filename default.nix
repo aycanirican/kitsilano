119627a5ecdadb4197ee7ce21b7783619cc58318
Aycan iRiCAN <iricanaycan@gmail.com>
2019-10-17 04:52:43 +0300

    Bump nixpkgs channel

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
 { pkgsPath ? (builtins.fetchGit {
-                name = "nixos-release-19.03";
+                name = "nixos-release-19.09";
                 url = https://github.com/nixos/nixpkgs-channels;
-                ref = "nixos-19.03";
+                ref = "nixos-19.09";
               })
 , isContainer ? false
 }:
 
 let
-  pkgs = import pkgsPath {
-    overlays = [ (import ./overlays/aa_emacs.nix) ];
-  };
+  pkgs         = import pkgsPath { overlays = [ (import ./overlays/aa_emacs.nix) ]; };
   dockerImages = import ./docker { inherit pkgs; };
 in rec {
   emacs    = import ./emacsImpl.nix    { inherit pkgs isContainer; };
